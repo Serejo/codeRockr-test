0f72a579557d198a5cdda57cdf3353e2bcf18d71
Erick Serejo <erickrainier@hotmail.com>
2022-10-04 16:24:01 -0300

    header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,11 @@
-import { UserCircle } from "phosphor-react";
-
 export function Header() {
   return (
-    <div className="flex justify-end  px-4 py-2 bg-slate-100 border-b border-slate-200">
-      <div className="flex justify-between">
-        <p>Usuário: Francisco</p>
-        {/* <UserCircle
-          size={25}
-          className="ml-3 hover:text-purple-800 hover:cursor-pointer"
-        /> */}
+    <header className="w-full py-4 flex justify-around bg-[#2D2D2D]">
+      <div className="flex text-white font-bold ">Rockr Blog</div>
+      <div className="flex text-white justify-between items-end">
+        <div className="px-7 mx-5">Posts</div>
+        <div className="px-7 mx-5">Contact</div>
       </div>
-    </div>
+    </header>
   );
 }
